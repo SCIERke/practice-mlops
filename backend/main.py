141cf00ff66ffb167d153c8075f4cffe16e62a76
Peerapat Setsuk <peerapatsetsuk15@gmail.com>
2025-07-26 12:55:58 +0700

    feat: create health check endpoint

--- backend/main.py
+++ backend/main.py
@@ -2,6 +2,10 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-@app.get("/")
-def read_root():
-  return {"status": "Yeah Yeah, It's work!"}
+# @app.get("/")
+# def read_root():
+#   return n
+
+@app.get("/health-check")
+def show_health_statu():
+  return {"status": "Yeah Yeah, It's worked!"}
