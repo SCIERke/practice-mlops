455ff93789328a075e89ddbe5db4caa50ba965ff
Peerapat Setsuk <peerapatsetsuk15@gmail.com>
2025-07-30 14:32:11 +0700

    Removes model loading and simplifies endpoint
    Removes the model loading functionality from the backend, along with associated dependencies.
    The chat endpoint now returns a placeholder message. This simplification is likely done for development or testing purposes.

--- backend/main.py
+++ backend/main.py
@@ -1,11 +1,9 @@
 from fastapi import FastAPI
-from contextlib import asynccontextmanager
 import os
 from dotenv import load_dotenv
 from fastapi.middleware.cors import CORSMiddleware
 
 from schemas.chat import MessagePayload
-from load_model import load_model
 
 load_dotenv()
 DEVICE = os.getenv('DEVICE', 'cpu').lower()
@@ -17,20 +15,7 @@ if DEVICE not in ['cpu', 'cuda']:
 
 model_bundle = {}
 
-@asynccontextmanager
-async def lifespan(app: FastAPI):
-    try:
-      model_bundle["model"] ,model_bundle["tokenizer"] = await load_model(MODEL_NAME , CACHE_DIR, DEVICE)
-    except Exception as e:
-      print(f"Error loading model: {e}")
-      raise e
-    print(f"Model and tokenizer loaded on {DEVICE}")
-    yield
-    print("Cleaning up model...")
-    model_bundle.clear()
-
-app = FastAPI(lifespan=lifespan)
-
+app = FastAPI()
 
 """
   DEV ONLY
@@ -57,20 +42,4 @@ def create_message(message_payload: MessagePayload):
   messages = [
       {"role": "user", "content": message_payload.content},
   ]
-  inputs = model_bundle['tokenizer'].apply_chat_template(
-    messages,
-    add_generation_prompt=True,
-    tokenize=True,
-    return_dict=True,
-    return_tensors="pt",
-  ).to(model_bundle['model'].device)
-
-  inputs = {k: v.to(model_bundle['model'].device) for k, v in inputs.items()}
-
-  outputs = model_bundle['model'].generate(
-    **inputs,
-    max_new_tokens=40,
-    pad_token_id=model_bundle['tokenizer'].pad_token_id or model_bundle['tokenizer'].eos_token_id
-    )
-  outputs_decode = model_bundle['tokenizer'].decode(outputs[0][inputs["input_ids"].shape[-1]:])
-  return {"message": outputs_decode}
+  return {"message": "outputs_decode"}
